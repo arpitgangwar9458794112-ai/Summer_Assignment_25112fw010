77fbc7c5803da96c032f8be1dab80fc82904ec8c
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-06-19 22:29:01 +0530

    Q46 Write a program to Write function for Armstrong

---
+++ summer assignment/Q46.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to check Armstrong number
+int isArmstrong(int num)
+{
+    int original, remainder, digits = 0;
+    int sum = 0, temp;
+
+    original = num;
+    temp = num;
+
+    // Count digits
+    while(temp != 0)
+    {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = num;
+
+    // Calculate sum of powers of digits
+    while(temp != 0)
+    {
+        remainder = temp % 10;
+        sum += pow(remainder, digits);
+        temp /= 10;
+    }
+
+    return (sum == original);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(isArmstrong(num))
+        printf("%d is an Armstrong Number.\n", num);
+    else
+        printf("%d is Not an Armstrong Number.\n", num);
+
+    return 0;
+}
