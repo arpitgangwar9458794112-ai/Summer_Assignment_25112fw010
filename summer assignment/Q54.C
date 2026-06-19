dc88e45f6b1ed5c0833185d85fb5c3df94ed02d2
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-06-19 22:46:54 +0530

    Q54 Write a program to Frequency of an element.

---
+++ summer assignment/Q54.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, element;
+    int count = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to find frequency: ");
+    scanf("%d", &element);
+
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == element)
+            count++;
+    }
+
+    printf("Frequency of %d = %d\n", element, count);
+
+    return 0;
+}
