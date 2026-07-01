91ffe5c88c126676613afd1cc6b9c280d0e341ed
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-07-01 09:43:42 +0530

    Q87 Write a program to Character frequency.

---
+++ summer assignment/Q87.C
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, count;
+    int visited[100] = {0};
+
+    printf("Enter a string: ");
+    scanf("%99s", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (visited[i] == 1)
+            continue;
+
+        count = 1;
+
+        for (j = i + 1; str[j] != '\0'; j++)
+        {
+            if (str[i] == str[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%c = %d\n", str[i], count);
+    }
+
+    return 0;
+}
