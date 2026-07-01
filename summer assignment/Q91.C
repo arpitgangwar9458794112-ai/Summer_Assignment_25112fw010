8df9a7fc4a8d54f835dd3ec321cace82ac5837bc
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-07-01 09:50:28 +0530

    Q91 Write a program to Check anagram strings

---
+++ summer assignment/Q91.C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    int freq1[256] = {0}, freq2[256] = {0};
+    int i, len1 = 0, len2 = 0;
+
+    printf("Enter first string: ");
+    scanf("%99s", str1);
+
+    printf("Enter second string: ");
+    scanf("%99s", str2);
+
+    while (str1[len1] != '\0')
+        len1++;
+
+    while (str2[len2] != '\0')
+        len2++;
+
+    if (len1 != len2)
+    {
+        printf("Strings are Not Anagrams.\n");
+        return 0;
+    }
+
+    for (i = 0; str1[i] != '\0'; i++)
+    {
+        freq1[(unsigned char)str1[i]]++;
+        freq2[(unsigned char)str2[i]]++;
+    }
+
+    for (i = 0; i < 256; i++)
+    {
+        if (freq1[i] != freq2[i])
+        {
+            printf("Strings are Not Anagrams.\n");
+            return 0;
+        }
+    }
+
+    printf("Strings are Anagrams.\n");
+
+    return 0;
+}
