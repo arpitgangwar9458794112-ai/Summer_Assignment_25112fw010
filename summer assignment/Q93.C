882641d59dd485571808155a25b935ec5cfe98c0
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-07-01 09:53:48 +0530

    Q93 Write a program to Check string rotation

---
+++ summer assignment/Q93.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100], str2[100], temp[200];
+
+    printf("Enter first string: ");
+    scanf("%99s", str1);
+
+    printf("Enter second string: ");
+    scanf("%99s", str2);
+
+    if (strlen(str1) != strlen(str2))
+    {
+        printf("Strings are Not Rotations.\n");
+        return 0;
+    }
+
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    if (strstr(temp, str2) != NULL)
+        printf("Strings are Rotations.\n");
+    else
+        printf("Strings are Not Rotations.\n");
+
+    return 0;
+}
