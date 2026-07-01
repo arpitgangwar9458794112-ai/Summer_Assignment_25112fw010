11ab5af089b63aa641cba180de494875b3a95c49
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-07-01 09:47:09 +0530

    Q89 Write a program to Find first non-repeating character.

---
+++ summer assignment/Q89.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, count;
+    int found = 0;
+
+    printf("Enter a string: ");
+    scanf("%99s", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        count = 0;
+
+        for (j = 0; str[j] != '\0'; j++)
+        {
+            if (str[i] == str[j])
+            {
+                count++;
+            }
+        }
+
+        if (count == 1)
+        {
+            printf("First non-repeating character = %c\n", str[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if (found == 0)
+    {
+        printf("No non-repeating character found.\n");
+    }
+
+    return 0;
+}
