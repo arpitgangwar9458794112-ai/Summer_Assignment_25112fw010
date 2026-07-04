d5e1d6dcc73dfc699e48b3e8d7c4d383667a88b6
arpitgangwar9458794112-ai <arpitgangwar9458794112@gmail.com>
2026-07-04 21:24:04 +0530

    Q101 Write a program to Create number guessing game.

---
+++ summer assignment/Q101.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number, guess, attempts = 0;
+
+    // Generate random number between 1 and 100
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    printf("===== Number Guessing Game =====\n");
+    printf("I have chosen a number between 1 and 100.\n");
+    printf("Try to guess it!\n");
+
+    do
+    {
+        printf("\nEnter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number)
+            printf("Too High! Try Again.\n");
+        else if (guess < number)
+            printf("Too Low! Try Again.\n");
+        else
+            printf("\nCongratulations! You guessed the correct number.\n");
+
+    } while (guess != number);
+
+    printf("You guessed it in %d attempts.\n", attempts);
+
+    return 0;
+}
